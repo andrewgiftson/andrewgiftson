22721f847454f682dd8cc42033e0bcd70bdcd2e1
andrewgiftson <andrewgiftson@gmail.com>
2022-05-23 19:09:35 +0530

    new

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,13 +14,14 @@ namespace ConsoleApp1
             //ReverseNumber reverseNumber = new ReverseNumber();
             //programmingknow1 programmingknow1 = new programmingknow1();
             // programmingknow1.fishorcrab();
-            testcheck1 test = new testcheck1();
-            test.testcheck();
+            plusMinus1.plusMinus(q);
             /*int n = Convert.ToInt32(Console.ReadLine().Trim());
 
             List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
             plusMinus.pM(arr);*/
+
+
 
         }
     }
--- ConsoleApp1/testcheck1.cs
+++ ConsoleApp1/testcheck1.cs
@@ -1,33 +1,63 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
+
 
 namespace ConsoleApp1
 {
     class testcheck1
     {
-        public  void testcheck()
+        public static void testcheck()
         {
-            int add1 = 0;
-            int add2 = 0;
-            int[,] ar = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            for(int i =0; i < ar.GetLength(0); i++)
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
             {
-                add1 += ar[i, i];
-            }
-            //------------------
-            int k = 0;
-            for(int j=(ar.GetLength(0)-1);j>=0;j--)
-            {
-                add2 += ar[j,k];
-                k++;
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
             }
 
-            Console.WriteLine(add1);
-            Console.WriteLine(add2);
+
+
+            //my program
+            int rowCount = arr[0].Count;
+            int diagonal1 = 0;
+            int diagonal2 = 0;
+            int k = 0;
+            int diff = 0;
+            for (int i = 0; i < rowCount; i++)
+            {
+                diagonal1 += arr[i][i];
+            }
+            for (int j = rowCount - 1; j >= 0; j--)
+            {
+                diagonal2 += arr[j][k];
+                k++;
+            }
+            if (diagonal1 > diagonal2)
+            {
+                diff = diagonal1 - diagonal2;
+            }
+            else
+            {
+                diff = diagonal2 - diagonal1;
+            }
+            Console.WriteLine( diff);
             Console.ReadLine();
         }
-    }
+        
+    }  
+    
 }
