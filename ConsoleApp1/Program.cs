4e68e89b739bd38a61d6cff18ad7de3e6e93a393
andrewgiftson <andrewgiftson@gmail.com>
2022-05-26 19:01:38 +0530

    camelcase4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public static void Main(string[] args)
+        /*public static void Main(string[] args)
         {
             //SpiralPrint spiralPrint = new SpiralPrint();
             //ReverseNumber reverseNumber = new ReverseNumber();
@@ -19,11 +20,53 @@ namespace ConsoleApp1
 
             List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
-            plusMinus.pM(arr);*/
+            plusMinus.pM(arr);
 
 
 
+        }*/
+        public static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            string[] inputs = { "S;M;plasticCup()", "C;V;mobile phone" };
+            string result = string.Empty;
+            foreach (var input in inputs)
+            {
+                string newInput = input.Replace("()", string.Empty);
+                string[] splitInput = newInput.Split(';');
+                char condition = char.Parse(splitInput[0]);
+                char type = char.Parse(splitInput[1]);
+                string name = splitInput[2];
+                if (condition.Equals('S'))
+                {
+                    Regex r = new Regex(@"(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z])(?=[A-Z])|(?<=[A-Za-z])(?=[^A-Za-z])");
+                    result = r.Replace(name, " ").ToLower();
+                }
+                else if (condition.Equals('C'))
+                {
+                    string[] nameSets = name.Split(' ');
+                    List<string> newNameSets = new List<string>();
+                    foreach (var nameSet in nameSets)
+                    {
+                        string newNameSet = char.ToUpper(nameSet[0]) + nameSet.Substring(1);
+                        newNameSets.Add(newNameSet);
+                    }
+                    result = String.Join("", newNameSets);
+                    if (type.Equals('V'))
+                    {
+                        result = char.ToLowerInvariant(result[0]) + result.Substring(1);
+                    }
+                    else if (type.Equals('M'))
+                    {
+                        result = char.ToLowerInvariant(result[0]) + result.Substring(1) + "()";
+                    }
+                }
+                Console.WriteLine(result);
+            }
         }
+
+
+
     }
-   
+
 }
