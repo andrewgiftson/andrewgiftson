f87e372924c6bd01fb6f4febe22840ccb6f87df2
Andrewgiftson <andrewgiftson@gmail.com>
2022-07-23 18:49:48 +0530

    prepare

--- ConsoleApp1/PermutingTwoArrays.cs
+++ ConsoleApp1/PermutingTwoArrays.cs
@@ -7,40 +7,40 @@ namespace ConsoleApp1
 {
     public class PermutingTwoArrays
     {
-        public static void twoarrays(string[] args)
-        {
-            int k = 10;
-            List<int> A = new List<int>();
-            List<int> B = new List<int>();
-            A.Add(2);
-            A.Add(1);
-            A.Add(3);
-            B.Add(7);
-            B.Add(8);
-            B.Add(7);
-            string result = twoArrays(k,A,B);
-            Console.WriteLine(result);
-            Console.ReadLine();
-        }
-        public static string twoArrays(int k, List<int> A, List<int> B)
-        {
-            int i;   
-            if (A.Count == B.Count)
-            {
-                for (i = 0; i < A.Count; i++)
-                {
-                  if((A[i] + B[i])<k)
-                    {
-                        return "YES";
-                    }
-                    else
-                    {
-                        return "NO";
-                    }                                    
-                }
-            }
-            return null;
+        //public static void twoarrays()
+        //{
+        //    int k = 10;
+        //    List<int> A = new List<int>();
+        //    List<int> B = new List<int>();
+        //    A.Add(2);
+        //    A.Add(1);
+        //    A.Add(3);
+        //    B.Add(7);
+        //    B.Add(8);
+        //    B.Add(7);
+        //    string result = twoArrays(k,A,B);
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
+        //public static string twoArrays(int k, List<int> A, List<int> B)
+        //{
+              
+        //    if (A.Count == B.Count)
+        //    {
+        //        for (int i = 0; i < A.Count; i++)
+        //        {
+        //          if((A[i] + B[i])<k)
+        //            {
+        //                return "YES";
+        //            }
+        //            else
+        //            {
+        //                return "NO";
+        //            }                                    
+        //        }
+        //    }
+        //    return null;
             
-        }
+        //}
     }
 }
---
+++ ConsoleApp1/prepare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     public class prepare
+    {
+        public static void Main(string[] args)
+        {
+            int n = 5;
+            int k = 1;
+            for (int i = 0;i < n; i++)
+            {                
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(k+++" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
