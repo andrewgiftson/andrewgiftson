0359e69e989c34e1791ac59c1735c77fa8fd0a5c
Andrewgiftson <andrewgiftson@gmail.com>
2022-08-18 23:14:28 +0530

    sale by march

--- ConsoleApp1/prepare.cs
+++ ConsoleApp1/prepare.cs
@@ -11,8 +11,8 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
             List<int> ar = new List<int>();
-            ar.Add(10); ar.Add(20); ar.Add(20); ar.Add(10); ar.Add(10); ar.Add(30); ar.Add(50); ar.Add(10); ar.Add(20);
-            int n = 9;
+            ar.Add(1); ar.Add(1); ar.Add(3); ar.Add(1); ar.Add(2); ar.Add(1); ar.Add(3); ar.Add(3); ar.Add(3);ar.Add(3);
+            int n = 10;
             int count = 0;
             for (int i = 0; i < n; i++)
             {
@@ -24,13 +24,29 @@ namespace ConsoleApp1
                         {
                             n -= 2;
                             count++;
+                            //Console.WriteLine("i=" + ar[i] + "," + i);
                             ar.Remove(ar[i]);
-                            ar.Remove(ar[j]);
+
+                            if (i > j || i==j)
+                            {
+                               // Console.WriteLine("j+1=" + ar[j+1]+","+(j+1));
+                                ar.Remove(ar[j+1]);
+                            }
+                            else if(i<j )
+                            {
+                                //Console.WriteLine("j-1=" + ar[j-1] + "," + (j-1));
+                                ar.Remove(ar[j - 1]);
+                            }
+                            
                         }
                     }
                 }
             }
-            Console.WriteLine(count);
+            for(int k=0;k<n;k++)
+            {
+                Console.WriteLine(ar[k]);
+            }
+            Console.WriteLine("Repeat = "+count);
              Console.ReadLine();
         }
     }
